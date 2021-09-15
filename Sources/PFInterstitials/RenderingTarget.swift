c0fd23bd5cafdc50bfe11824d8128e47b1e6ae36
theRealRobG <robertnw5@gmail.com>
2021-09-15 02:32:10 +0100

    Added RenderingTarget
    The idea behind this protocol is that the event controller
    polyfill will not actually be able to switch between player
    instances without having access to where the player is
    rendering to. So this will have to be provided by the consumer.
    Example implementations could be switching player on
    AVPlayerViewController, or switching player on an AVPlayerLayer.

---
+++ Sources/PFInterstitials/RenderingTarget.swift
@@ -0,0 +1,12 @@
+//
+//  RenderingTarget.swift
+//  
+//
+//  Created by Robert Galluccio on 15/09/2021.
+//
+
+import AVFoundation
+
+public protocol RenderingTarget {
+    func updatePlayerReference(player: AVPlayer)
+}
