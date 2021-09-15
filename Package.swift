11ac21062d8d03aa01fec48c759f05576adb1c21
theRealRobG <robertnw5@gmail.com>
2021-09-15 02:06:12 +0100

    Updated Package.swift formatting

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PFInterstitials",
-            targets: ["PFInterstitials"]),
+            targets: ["PFInterstitials"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,9 +21,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PFInterstitials",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "PFInterstitialsTests",
-            dependencies: ["PFInterstitials"]),
+            dependencies: ["PFInterstitials"]
+        ),
     ]
 )
